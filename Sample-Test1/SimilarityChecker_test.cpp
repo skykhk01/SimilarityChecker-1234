737a8e06f6355ce7d9ba6fb0237f7c78d4161fbc
hwangyu.kim <skykhk01@gmail.com>
2023-06-02 13:37:29 +0900

    Add feature/length code

--- Sample-Test1/SimilarityChecker_test.cpp
+++ Sample-Test1/SimilarityChecker_test.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
 #include "../SimilarityChecker/SimilarityChecker.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
+TEST(SimilarityChecker, TestName) {
+	SimilarityChecker similarityChecker;
+	EXPECT_THROW(similarityChecker.getLengthCheckScore("A", ""), std::invalid_argument);
+	EXPECT_THROW(similarityChecker.getLengthCheckScore("", "B"), std::invalid_argument);
+	EXPECT_EQ(similarityChecker.getLengthCheckScore("ASD", "DSA"), 60);
+	EXPECT_EQ(similarityChecker.getLengthCheckScore("A", "BB"), 0);
+	EXPECT_EQ(similarityChecker.getLengthCheckScore("AAABB", "BAA"), 20);
+	EXPECT_EQ(similarityChecker.getLengthCheckScore("AA", "AAA"), 30);
 }
--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -1,5 +1,56 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
 class SimilarityChecker
 {
 public:
+	int getLengthCheckScore(const string& leftStr, const string& rightStr)
+	{
+		assertIllegalArgument(leftStr, rightStr);
 
+		if (leftStr.length() == rightStr.length()) return MAX_LENGTH_CHECK_SCORE;
+		if (leftStr.length() >= 2 * rightStr.length()) return 0;
+		if (rightStr.length() >= 2 * leftStr.length()) return 0;
+
+		return getPartialScore(leftStr, rightStr);
+	}
+
+private:
+	void assertIllegalArgument(const string& leftStr, const string& rightStr)
+	{
+		if (leftStr == "" || rightStr == "")
+		{
+			throw std::invalid_argument("글자를 입력해주세요");
+		}
+	}
+
+	int getLengthGap(const string& leftStr, const string& rightStr)
+	{
+		if (leftStr.length() > rightStr.length()) 
+			return leftStr.length() - rightStr.length();
+
+		return rightStr.length() - leftStr.length();
+	}
+
+	int getShortStringLength(const string& leftStr, const string& rightStr)
+	{
+		if (leftStr.length() > rightStr.length()) 
+			return rightStr.length();
+
+		return leftStr.length();
+	}
+
+	int getPartialScore(const string& leftStr, const string& rightStr)
+	{
+		int gap = getLengthGap(leftStr, rightStr);
+		int shortStringLength = getShortStringLength(leftStr, rightStr);
+
+		return (1 - ((double)gap / (double)shortStringLength)) * MAX_LENGTH_CHECK_SCORE;
+	}
+
+private:
+	const int MAX_LENGTH_CHECK_SCORE = 60;
 };
